7012428361d6fbf5947dbb47c413309339298798
Ilya Shilov <ilyashilov@MacBook-Pro-162-2021.local>
2023-10-08 12:16:26 +0300

    v1.0.0a-ea2b2676c28c0db26d39331a336c6b92

---
+++ website/website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-==e54j*zqfi^11ti7j_l^im*v4#vhw_n0gekm8re59rnie=$(("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
